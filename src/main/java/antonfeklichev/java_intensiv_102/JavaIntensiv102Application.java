d5fe3a90b4c17b42436d42ff300f1dc585e8bbcc
Anton <afeklichev@yandex.ru>
2025-03-09 18:12:24 +0300

    added CustomException

--- src/main/java/antonfeklichev/java_intensiv_102/JavaIntensiv102Application.java
+++ src/main/java/antonfeklichev/java_intensiv_102/JavaIntensiv102Application.java
@@ -1,13 +1,20 @@
 package antonfeklichev.java_intensiv_102;
 
+import antonfeklichev.java_intensiv_102.service.ExceptionService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JavaIntensiv102Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaIntensiv102Application.class, args);
-	}
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context = SpringApplication.run(JavaIntensiv102Application.class, args);
+
+        ExceptionService exceptionService = context.getBean(ExceptionService.class);
+
+        exceptionService.methodThatCatchesException();
+    }
 
 }
